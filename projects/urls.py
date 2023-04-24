97138ef0d94a5b82b9d91533aff9cc762e236e9e
wsrn829 <wsrn829@gmail.com>
2023-04-24 15:52:37 -0700

    Feature 5 complete

---
+++ projects/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from projects.views import list_projects
+
+
+urlpatterns = [
+    path("", list_projects, name="list_projects"),
+]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from projects.models import Project
+
 
 # Create your views here.
+def list_projects(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects,
+    }
+    return render(request, "projects/list_projects.html", context)
