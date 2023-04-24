cc4596cb39cea5e59e557230a9006f5e6f6d095b
wsrn829 <wsrn829@gmail.com>
2023-04-24 16:44:20 -0700

    Feature 6 complete

--- tracker/urls.py
+++ tracker/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
+
+def redirect_to_home(request):
+    return redirect("list_projects")
+
+
 urlpatterns = [
+    path("", redirect_to_home, name="home"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
 ]
