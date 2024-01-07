b3c1b34339d5f3d554830707eaf36d4eeeb47815
wsrn829 <wsrn829@gmail.com>
2024-01-07 14:16:24 -0500

    Update project urls.py

--- tracker/urls.py
+++ tracker/urls.py
@@ -34,7 +34,7 @@ def redirect_to_home(request):
 
 urlpatterns = [
     path("", redirect_to_home, name="home"),
-    path("", project_root_view, name="project_root_view"),
+    # path("", project_root_view, name="project_root_view"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("accounts/", include("accounts.urls")),
