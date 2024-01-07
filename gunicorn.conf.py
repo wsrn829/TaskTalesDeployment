e07744c2263e3c6f75cfaefdf26dd16d33de0938
wsrn829 <wsrn829@gmail.com>
2024-01-07 12:51:00 -0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,6 @@
 bind = "0.0.0.0:8000"
 workers = 3
 reload = True
-module = "TaskTalesDeployment.wsgi:application"
+# module = "TaskTalesDeployment.wsgi:application"
+module = "tracker.wsgi:application"
+
