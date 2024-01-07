8a38374e7ab6356673b966571aec5cf849cd258e
wsrn829 <wsrn829@gmail.com>
2024-01-07 12:59:23 -0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,9 @@
-bind = "0.0.0.0:8000"
+# bind = "0.0.0.0:8000"
+# gunicorn.conf.py
+# gunicorn.conf.py
+import os
+
+bind = "0.0.0.0:" + os.environ.get("PORT", "8000")
 workers = 3
 reload = True
 # module = "TaskTalesDeployment.wsgi:application"
