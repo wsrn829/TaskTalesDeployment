e17b8ed040b9907c0207225756d8a9a99d20fe5e
wsrn829 <wsrn829@gmail.com>
2024-01-07 13:24:29 -0500

    Update project settings.py, wsgi.py, requirements.txt

--- tracker/settings.py
+++ tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,11 +127,11 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "staticfiles",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/images/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 
--- tracker/wsgi.py
+++ tracker/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracker.settings")
 
+
 application = get_wsgi_application()
+application = WhiteNoise(application)
+
+
 
 
