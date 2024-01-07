ec2869890ef9687aa48fa8ca50b49b755dda1c27
wsrn829 <wsrn829@gmail.com>
2024-01-06 22:29:47 -0500

    Update debug False

--- tracker/settings.py
+++ tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "https://tasktales-e12d965b0fbc.herokuapp.com/",
