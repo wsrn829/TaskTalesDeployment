2e243b042103a0c814e06a9babd22e9d4fcf438a
wsrn829 <wsrn829@gmail.com>
2024-01-07 14:33:49 -0500

    Update project settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -127,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 # [
 #     BASE_DIR / "staticfiles",
