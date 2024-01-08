29930a31937544e50f08e774ab20912f0965be3f
wsrn829 <wsrn829@gmail.com>
2024-01-08 17:29:27 -0500

    Fix static

--- tracker/settings.py
+++ tracker/settings.py
@@ -25,7 +25,8 @@ from whitenoise.middleware import WhiteNoiseMiddleware
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -151,7 +152,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 # [
 #     BASE_DIR / "staticfiles",
 #     # os.path.join(BASE_DIR, 'static'),
