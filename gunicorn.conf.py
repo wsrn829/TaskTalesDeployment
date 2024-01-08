fc95cf2a9369e90bcd5c807357ee7f5b2710d13f
wsrn829 <wsrn829@gmail.com>
2024-01-07 19:26:19 -0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,31 @@
-# bind = "0.0.0.0:8000"
-# gunicorn.conf.py
-# gunicorn.conf.py
 import os
+from tracker.wsgi import application
 
 bind = "0.0.0.0:" + os.environ.get("PORT", "8000")
 workers = 3
 reload = True
-# module = "TaskTalesDeployment.wsgi:application"
-module = "tracker.wsgi:application"
 
+class FaviconMiddleware:
+    def __init__(self, app, default_favicon_path='favicon.ico'):
+        self.app = app
+        self.default_favicon_path = default_favicon_path
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'] == '/favicon.ico':
+            try:
+                current_dir = os.path.dirname(__file__)
+                favicon_path = os.path.join(current_dir, self.default_favicon_path)
+
+                with open(favicon_path, 'rb') as f:
+                    favicon_data = f.read()
+
+                start_response('200 OK', [('Content-Type', 'image/x-icon')])
+                return [favicon_data]
+            except FileNotFoundError:
+                start_response('404 Not Found', [('Content-Type', 'text/plain')])
+                return [b'Not Found']
+
+        return self.app(environ, start_response)
+
+# Wrap the main application with FaviconMiddleware
+application = FaviconMiddleware(application)
