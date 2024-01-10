a7d9be9d3c39d9b91a9d0238353a39c952c9e385
wsrn829 <wsrn829@gmail.com>
2024-01-09 19:29:01 -0500

    Comment out static configs in settings.py

--- tracker/settings.py
+++ tracker/settings.py
@@ -114,10 +114,10 @@ USE_TZ = True
 
 
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = "/media/"
